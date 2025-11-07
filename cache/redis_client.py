a227500ca24bd35a02eb424dba1b59da38604fb8
William Dinh <hoangdinh2411@gmail.com>
2025-11-07 13:28:17 +0100

    ping redis when connect

--- cache/redis_client.py
+++ cache/redis_client.py
@@ -9,7 +9,12 @@ rds: Optional[redis.Redis] = None
 async def get_redis() -> redis.Redis:
     global rds
     if rds is None:
-        rds = redis.from_url(REDIS_URL, decode_responses=True)
+        rds = redis.from_url(REDIS_URL,
+                             decode_responses=True,
+                             max_connections=10,
+                            socket_keepalive=True,)
+        pong = await rds.ping()
+        print("[Redis] URL:", REDIS_URL.split("@")[-1], "PING:", pong)
     return rds
 
 
